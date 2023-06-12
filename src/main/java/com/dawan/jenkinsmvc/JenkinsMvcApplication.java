b7be34d73384a6002b630a004b6352a74fa3fdd4
Yanis Adekalom <yanis971.60@gmail.com>
2023-06-12 17:12:21 +0200

    JavaDoc

--- src/main/java/com/dawan/jenkinsmvc/JenkinsMvcApplication.java
+++ src/main/java/com/dawan/jenkinsmvc/JenkinsMvcApplication.java
@@ -3,9 +3,17 @@ package com.dawan.jenkinsmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The type Jenkins mvc application.
+ */
 @SpringBootApplication
 public class JenkinsMvcApplication {
 
+	/**
+	 * The entry point of application.
+	 *
+	 * @param args the input arguments
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsMvcApplication.class, args);
 	}
--- src/main/java/com/dawan/jenkinsmvc/controller/RootController.java
+++ src/main/java/com/dawan/jenkinsmvc/controller/RootController.java
@@ -3,8 +3,16 @@ package com.dawan.jenkinsmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * The type Root controller.
+ */
 @Controller
 public class RootController {
+    /**
+     * Index string.
+     *
+     * @return the string
+     */
     @GetMapping()
     public String index() {
         //throw new RuntimeException("Fail voluntarily");
