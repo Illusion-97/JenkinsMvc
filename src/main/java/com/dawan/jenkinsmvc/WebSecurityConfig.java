e89573c217093f6aa9d36cd93292d4fb886089bf
Yanis Adekalom <yanis971.60@gmail.com>
2023-06-14 12:59:51 +0200

    Jacoco

--- src/main/java/com/dawan/jenkinsmvc/WebSecurityConfig.java
+++ src/main/java/com/dawan/jenkinsmvc/WebSecurityConfig.java
@@ -13,7 +13,7 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests((request) -> request.anyRequest().permitAll())
+                .authorizeHttpRequests(request -> request.anyRequest().permitAll())
                 .logout(LogoutConfigurer::permitAll)
                 .build();
     }
